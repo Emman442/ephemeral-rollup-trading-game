6f99b10dea089222c0ad0840f727cf5a83ab32a9
Emmanuel Ndema <ndemaemmanuel82@gmail.com>
2025-10-14 11:39:52 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@s

--- src/components/providers/WalletProvider.tsx
+++ src/components/providers/WalletProvider.tsx
@@ -6,10 +6,8 @@ import {
   WalletProvider as SolanaWalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import { SolflareWalletAdapter } from "@solana/wallet-adapter-solflare";
 import { clusterApiUrl } from "@solana/web3.js";
 import type { FC, ReactNode } from "react";
 import { useMemo } from "react";
@@ -19,7 +17,7 @@ export const WalletProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
   const wallets = useMemo(
     () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
-    [network]
+    []
   );
 
   return (
